1a1c22336a0c3d07fea376145ca5067394cee793
Jenish Mariyajerald <jenish.mariyajerald@LT-MAC-197.local>
2023-05-10 09:19:24 +0530

    Added types & style

--- src/screen/chat/index.tsx
+++ src/screen/chat/index.tsx
@@ -1,32 +1,18 @@
-import React, {useEffect, useState} from 'react';
-
+import React, { useEffect, useState } from 'react';
 import {
   View,
   Text,
   TextInput,
   Button,
-  StyleSheet,
   ScrollView,
 } from 'react-native';
-
-//import { createOpenAIInstance, OpenAI } from '../OpenAI/openai';
-
-import {Configuration, OpenAIApi} from 'openai';
+import { Configuration, OpenAIApi } from 'openai';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
-// const openaiApiKey = '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';
-
-//const openai: OpenAI = createOpenAIInstance(openaiApiKey);
-
-interface Message {
-  content: string;
-
-  sender: 'user' | 'ai';
-}
+import chatStyle from './styles';
+import { MessageProps } from './types'
 
 const Chat = () => {
-  const [messages, setMessages] = useState<Message[]>([]);
-
+  const [messages, setMessages] = useState<MessageProps[]>([]);
   const [inputText, setInputText] = useState('');
   const [apiKey, setApiKey] = useState('');
   useEffect(() => {
@@ -37,7 +23,6 @@ const Chat = () => {
       const value = await AsyncStorage.getItem('user');
       if (value !== null) {
         const val = JSON.parse(value);
-        console.log('APIkey', val.apikey);
         setApiKey(val.apikey);
       }
     } catch (error) {
@@ -45,18 +30,10 @@ const Chat = () => {
     }
   };
 
-  const handleSendMessage = async () => {
-    getApiKey();
-    //try {
-
-    // Send the user's message to OpenAI
-
+  const handleSendMessage = async () => {  
     const prompt = inputText;
-
-    const config = new Configuration({apiKey: apiKey});
-
+    const config = new Configuration({ apiKey: apiKey });
     const openai = new OpenAIApi(config);
-
     const completetion = {
       model: 'text-davinci-002',
       prompt: prompt,
@@ -65,40 +42,33 @@ const Chat = () => {
     };
 
     const response = await openai.createCompletion(completetion);
-
     const reply = response.data.choices[0].text;
-
-    console.log('reply', reply); //Update the messages state with the user's input and OpenAI's reply
-
     setMessages((prevMessages: any) => [
       ...prevMessages,
-
-      {content: inputText, sender: 'user'},
-
-      {content: reply, sender: 'ai'},
+      { content: inputText, sender: 'user' },
+      { content: reply, sender: 'ai' },
     ]);
-
-    setInputText(''); // } catch (error) { //   if (error.response?.status === 429) { //     const retryAfter = error.response.headers['retry-after']; //     const retryAfterSeconds = parseInt(retryAfter, 10); //     // Wait for the specified number of seconds before retrying //     await new Promise((resolve) => setTimeout(resolve, retryAfterSeconds * 2000)); //     // Retry the request //     await handleSendMessage(); // Adjusted to use `await` to wait for the retry //   } else { //     // Handle other errors //   } // }
+    setInputText('');
   };
 
   return (
-    <View style={styles.container}>
-      <ScrollView style={styles.messageContainer}>
+    <View style={chatStyle.container}>
+      <ScrollView style={chatStyle.messageContainer}>
         {messages.map(message => (
           <View
             key={message.id}
             style={[
-              styles.messageBubble,
+              chatStyle.messageBubble,
 
-              message.sender === 'user' ? styles.userBubble : styles.botBubble,
+              message.sender === 'user' ? chatStyle.userBubble : chatStyle.botBubble,
             ]}>
-            <Text style={styles.messageText}>{message.content}</Text>
+            <Text style={chatStyle.messageText}>{message.content}</Text>
           </View>
         ))}
       </ScrollView>
-      <View style={styles.inputContainer}>
+      <View style={chatStyle.inputContainer}>
         <TextInput
-          style={styles.textInput}
+          style={chatStyle.textInput}
           value={inputText}
           onChangeText={setInputText}
         />
@@ -108,66 +78,6 @@ const Chat = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
 
-    justifyContent: 'center',
-
-    padding: 16,
-  },
-
-  messageContainer: {
-    flex: 1,
-  },
-
-  messageBubble: {
-    paddingHorizontal: 12,
-
-    paddingVertical: 8,
-
-    borderRadius: 8,
-
-    marginBottom: 8,
-
-    maxWidth: '80%',
-  },
-
-  userBubble: {
-    backgroundColor: 'lightblue',
-
-    alignSelf: 'flex-end',
-  },
-
-  botBubble: {
-    backgroundColor: 'lightgray',
-
-    alignSelf: 'flex-start',
-  },
-
-  messageText: {
-    fontSize: 16,
-  },
-
-  inputContainer: {
-    flexDirection: 'row',
-
-    alignItems: 'center',
-  },
-
-  textInput: {
-    flex: 1,
-
-    marginRight: 8,
-
-    borderWidth: 1,
-
-    borderRadius: 8,
-
-    paddingVertical: 4,
-
-    paddingHorizontal: 8,
-  },
-});
 
 export default Chat;
